ff5ed3a79ca0af4a20fad1853dad5ed2a3f01564
tahapirhadii <tp64547@gmail.com>
2025-07-27 02:40:58 +0330

    add telegram bot code and requirements.text

---
+++ telegram-weather.py
@@ -0,0 +1,30 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+import requests
+
+TOKEN = "8453202292:AAGV5X6yAID40FmZHy4SnCnbfeSzlnYgoVc"
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Hi! Send me the name of the city to get the weather.")
+
+def get_weather(city_name):
+    # Placeholder for future weather API integration
+    return f"Got the city {city_name}. Weather info will be here soon."
+
+def handle_message(update: Update, context: CallbackContext):
+    city = update.message.text
+    weather_report = get_weather(city)
+    update.message.reply_text(weather_report)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+
+    updater.start_polling()
+    updater.idle()
+
+if name == '__main__':
+    main()
